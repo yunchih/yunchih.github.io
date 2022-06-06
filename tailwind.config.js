fa8a8aab8db73b77c5496b51bc78c2920e2029df
hugcis <hmj.cisneros@gmail.com>
2022-06-06 17:06:36 +0200

    Progress in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,35 @@ module.exports = {
   theme: {
     container: {
       center: true,
+      screens: {
+        sm: '40rem',
+        md: '40rem',
+        lg: '40rem',
+        xl: '45rem',
+        '2xl': '50rem',
+      },
+    },
+    letterSpacing: {
+      wide: ".03em"
     },
     extend: {
+      lineHeight: {
+        'loose': '1.7',
+      },
       flexGrow: {
         '2': 2
       },
             flexShrink: {
         '2': 2
-      }
-
+      },
+      colors: {
+        'tp-black': 'rgba(0,0,0,.8)',
+      },
+      fontFamily: {
+        sans: ["-apple-system", "BlinkMacSystemFont", "avenir next", "avenir",
+               "segoe ui", "helvetica neue", "helvetica",
+               "Cantarell", "Ubuntu", "roboto", "noto", "arial", "sans-serif"],
+      },
     }
   },
   plugins: [],
